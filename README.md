f1d02f373c41cdc48a74278c842ad9b14584eb36
Sergey Aksenov <sergey.aksenov@aiven.io>
2026-05-29 12:39:05 +1200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+eicar
+The source code for this example contains the EICAR test string, which will be used to test YARA rule evaluation.
---
+++ eicar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import time
+
+# This line should be identified as the EICAR test string by antivirus software.
+_ = r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+
+if __name__ == '__main__':
+    while True:
+      print("Muahahahaha! I'm evil! All your base now benong to us", flush=True)
+      time.sleep(2)
